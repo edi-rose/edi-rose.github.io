b83f611baaee1c7dd07f1d6ac2410305d5a8c50e
Edi Rose <edirose1998@gmail.com>
2018-01-03 15:56:05 +1300

    moved calc

---
+++ js-calculator/calc.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', turnOn)
+// var calculator = [
+//   display:'0',
+//   equation: []
+//
+// ]
+
+
+function turnOn ()
+{
+
+document.getElementById('display').innerHtml = "<p>0</p>";
+
+//   document.addEventListener("click", solve)
+//
+//   solve(first, adpter, second, d) =
+//   {
+// var first = '';
+// var adapter= '';
+// var second= '';
+//   // figure out how to set value a, b and c to the calculator buttons;
+// set(answer = first adapter second)
+// }
+
+}
